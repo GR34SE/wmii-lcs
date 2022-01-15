85e4a25161111cc6e2ec5d679687ff15fceefd6a
Adrian Celczynski <adriancelczynski@icloud.com>
2022-01-15 15:53:25 +0100

    Change substring keyword to subsequence

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # coding=utf-8
 
 # runtime and memory complexity is O(first_string * second_string)
-def get_longest_common_substring(first_string, second_string):
-    longest_common_substring_length = 0
+def get_longest_common_subsequence(first_string, second_string):
+    longest_common_subsequence_length = 0
 
     first_string_length = len(first_string)
     second_string_length = len(second_string)
@@ -22,22 +22,22 @@ def get_longest_common_substring(first_string, second_string):
             if first_string[i - 1] == second_string[j - 1]:
                 results_matrix[i][j] = results_matrix[i - 1][j - 1] + 1
 
-            # check if "better" result has been found, if so then update new longest substring and ending index
-            if results_matrix[i][j] > longest_common_substring_length:
-                longest_common_substring_length = results_matrix[i][j]
+            # check if "better" result has been found, if so then update new longest subsequence and ending index
+            if results_matrix[i][j] > longest_common_subsequence_length:
+                longest_common_subsequence_length = results_matrix[i][j]
                 last_found_index_in_first_string = i
 
-    # (length, longest_substring)
-    return longest_common_substring_length, first_string[last_found_index_in_first_string - longest_common_substring_length: last_found_index_in_first_string]
+    # (length, longest_subsequence)
+    return longest_common_subsequence_length, first_string[last_found_index_in_first_string - longest_common_subsequence_length: last_found_index_in_first_string]
 
 
 if __name__ == '__main__':
     # empty cases
-    print get_longest_common_substring("", "abc")  # - (0)
-    print get_longest_common_substring("abc", "")  # - (0)
-    print get_longest_common_substring("abcdefghijk", "3453453453")  # - (0)
+    print get_longest_common_subsequence("", "abc")  # - (0)
+    print get_longest_common_subsequence("abc", "")  # - (0)
+    print get_longest_common_subsequence("abcdefghijk", "3453453453")  # - (0)
 
     # optimistic cases
-    print get_longest_common_substring("abcdefghijkplmnjki", "def_hijk")  # def (3)
-    print get_longest_common_substring("___12345___", "345")  # 345 (3)
-    print get_longest_common_substring("ab1122", "xxx_ab1_xxx")  # ab1 (3)
+    print get_longest_common_subsequence("abcdefghijkplmnjki", "def_hijk")  # def (3)
+    print get_longest_common_subsequence("___12345___", "345")  # 345 (3)
+    print get_longest_common_subsequence("ab1122", "xxx_ab1_xxx")  # ab1 (3)
